6f3e60b9ffef0611bf19e2492f0997045d9895c9
niamb2023 <niamb2023@headroyce.org>
2024-02-02 11:26:08 -0500

    error in the filenames but new code as of feb 2

--- Marathon Data /marathon.py
+++ Marathon Data /marathon.py
@@ -16,7 +16,7 @@ import csv
 import os
 import seaborn as sns
 import matplotlib.pyplot as plt
-from utils import read_csv,lst_to_dct
+from utils import read_csv,lst_to_dct,median
 
 FILENAME = "/Users/niambashambu/Desktop/DS 2500/Marathon Data /boston_marathon_2022.csv"
 BIB_HEADER = "BibNumber"
@@ -24,6 +24,8 @@ AGE_HEADER = "AgeOnRaceDay"
 RANK_HEADER = "RankOverall"
 GENDER_HEADER = "Gender"
 TIME_HEADER = "OfficialTime"
+NAME_HEADER = 'FullName'
+DIR = "files"
 
 
 
@@ -37,26 +39,57 @@ def filter_age(target_age, ages, times):
         if ages[i] >= target_age:
             filtered_times.append(times[i])
     return filtered_times
+def get_filename(dirname, ext = ".csv"):
+    '''give a directory name (string), 
+    return the full path and name 
+    for every non directory file in the directory (list of strings)
+    '''
+    filenames = []
+    files = os.listdir(dirname)
+    for file in files:
+        if not os.path.isdir(file) and file.endswith(ext):
+            filenames.append(dirname + "/"+ file)
+    return filenames
 
 def main():
     # Gather data - read in the MBTA speed restrictions as a 2d list
     # and then convert to a dictionary where keys come from the header
-    data = read_csv(FILENAME)
+    data = read_csv(DIR + '/' +FILENAME)
     dct = lst_to_dct(data)
-    print(dct)    
+    #print(dct)    
     
     # pull out a list of ages
     ages = dct[AGE_HEADER]
     for i in range (len(ages)):
         ages[i] = int(ages[i])
-    print(f"Ages of runners in 2022: {ages}")
+    
+    #iteraing by value
+        #use listslicing to look at every 20th element intstead of everyone
+    for age in ages[::20]:
+        print(f"Someone is {age} years old")
     
     # filter to keep everyone older than me
-    master_times = filter_age(45, ages, dct[TIME_HEADER])
-    print(f"Times of everyone 45 or older: {master_times}")
+    #master_times = filter_age(45, ages, dct[TIME_HEADER])
+    #print(f"Times of everyone 45 or older: {master_times}")
+    
+    med = median(ages)
+    mean = sum(ages)/len(ages)
+    print(f"Median age: {med}, mean age: {round(mean,3)}")
     
     
-    
+
+    #read everyfile in a given directory without knowing its name
+    filenames = get_filename(DIR)
+    print(filenames)
+
+    names = []
+    for filename in filenames:
+        lst = read_csv(filename)
+        dct = lst_to_dct(lst)
+        names+= dct[NAME_HEADER]
+    print(f"{len(names)} finsihers over two years")
+    print(f"{len(set(names))} individuals over two years")
+
    
     
     
--- Marathon Data /utils.py
+++ Marathon Data /utils.py
@@ -8,15 +8,13 @@ def read_csv(filename):
     ''' given the name of a csv file, return its contents as a 2d list,
         including the header.'''
     data = []
+    
     with open(filename, "r") as infile:
         csvfile = csv.reader(infile)
         for row in csvfile:
             data.append(row)
     return data
-''' dct = {}
-    for row in lst:
-        dct[row[0]]=row[1:]
-    return dct'''
+
 
 def lst_to_dct(lst):
     dct = {}
@@ -28,3 +26,15 @@ def lst_to_dct(lst):
         for i in range(len(row)):
             dct[lst[0][i]].append(row[i])
     return dct
+
+def median(orig_lst):
+    ''' given a list of numbers, compute and return median'''
+    lst = orig_lst.copy()
+    lst.sort()
+    mid = len(lst) // 2
+    if len(lst) % 2 == 1:
+        return lst[mid]
+    else:
+        avg = (lst[mid] + lst[mid - 1]) / 2
+        return avg
+
