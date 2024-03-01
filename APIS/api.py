4adcc8b30696453526fcc00f97a315e79c205d47
niamb2023 <niamb2023@headroyce.org>
2024-03-01 11:01:41 -0500

    api calss note

--- APIS/api.py
+++ APIS/api.py
@@ -6,8 +6,55 @@ Json data, two ways:
 2. call API fucniton, print out upcoming temps
 
 
+API KEY: ffed0167b14f7d884fae933b4c1fc551
+
+
 '''
 
 import json
 import requests
+FILENAME = "/Users/niambashambu/Desktop/DS 2500/APIS/forecast.json"
+API_KEY = "d117fb50308d8c3422546985be308bba"
+URL = "https://api.openweathermap.org/data/2.5/forecast}"
 
+def main():
+    '''
+    #part one = read in JSON data from a file
+
+    with open(FILENAME,"r") as jfile:
+        data = json.load(jfile)
+
+    #data
+    #print(data)
+        
+    #whhat are the upcoming temps?
+    for d in data["list"]:
+        print(d["main"]["temp"])
+        print(d["weather"][0]["description"])
+
+'''
+    #specify location?
+    #parameters:lat,lon
+    #make the tmps fahrenheit
+    #parameter units ("units")
+    lat = float(input("what latitude?\n"))
+    lon = float(input("what longitude?\n"))
+
+
+    #get the weather in that location
+
+    params = {"lat": lat,
+              "lon":lon,
+              "appid": API_KEY,
+              "units":"imperial"}
+    
+    response = requests.get(URL,params = params)
+    data = response.json()
+    print(data)
+    for d in data["list"][:10]:
+        print(d["main"]["temp"])
+        print(d["weather"][0]["description"])
+
+
+if __name__ == "__main__":
+    main()
