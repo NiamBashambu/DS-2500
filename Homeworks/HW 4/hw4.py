f1c9c51563e36664d786dc9e475184e7c8229184
niamb2023 <niamb2023@headroyce.org>
2024-03-29 15:08:51 -0400

    starting hw 4

---
+++ Homeworks/HW 4/hw4.py
@@ -0,0 +1,17 @@
+
+
+
+import json
+import requests
+import pandas as pd
+import numpy as np
+import json
+import scipy.spatial.distance as ssd
+import matplotlib.pyplot as plt
+
+
+API_KEY = "48828f97cb2547a484a46134f02ba5b5"
+
+URL = "https://newsapi.org/v2/everything?q=keyword&apiKey=48828f97cb2547a484a46134f02ba5b5"
+
+
